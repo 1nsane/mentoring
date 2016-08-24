05df93ef2a75b052b355e51b04db1decc75af5cd
Yevgeniy Vtulkin <yevgeniy_vtulkin@epam.com>
2016-08-24 12:58:17 +0300

    rewrote without using creating new objects (runnable tasks apart)

--- task3/src/main/java/edu/epam/mentoring/task3/Server.java
+++ task3/src/main/java/edu/epam/mentoring/task3/Server.java
@@ -9,17 +9,20 @@ import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Server {
+    private static final int POOL_SIZE = 50;
+    private static final int BYTES_PER_THREAD = 2000;
+    private static final int BUFFER_SIZE = POOL_SIZE * BYTES_PER_THREAD * 2;
 
-//    private static final Set<String> cache = new HashSet<>();
+    private final byte[] bytes = new byte[BUFFER_SIZE];
 
     private int port;
     private ServerSocket server;
     private Socket socket;
     private DataOutputStream os;
     private DataInputStream is;
-    private ReentrantLock lock = new ReentrantLock();
-    private BlockingQueue<Runnable> tasks = new ArrayBlockingQueue<>(10);
-    private ExecutorService service = Executors.newFixedThreadPool(5);
+//    private ReentrantLock lock = new ReentrantLock();
+    private BlockingQueue<Runnable> tasks = new ArrayBlockingQueue<>(POOL_SIZE);
+    private ExecutorService service = Executors.newFixedThreadPool(POOL_SIZE);
 
     public Server(int port) {
         this.port = port;
@@ -44,7 +47,6 @@ public class Server {
             });
         }
 
-
         while (true) {
             readMessage();
         }
@@ -54,49 +56,47 @@ public class Server {
         int count = is.readInt();
         for (int i = 0; i < count; i++) {
             int length = is.readInt();
-            byte[] bytes = new byte[length];
-            is.readFully(bytes);
-            int index = i;
+            int index = (i % (POOL_SIZE * 2));
+            int offset = index * BYTES_PER_THREAD;
+            is.readFully(bytes, offset, length);
             try {
-                tasks.put(() -> calcSentence(bytes, index));
+                tasks.put(() -> calcSentence(offset));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private void calcSentence(byte[] bytes, int index) {
-//        String str = new String(bytes).intern();
-        String str = new String(bytes);
+    private void calcSentence(int offset) {
         int big = 0;
         int small = 0;
-        for (String word : str.split(" ")) {
-            if (word.length() <= 4) {
-//                cache.add(word);
-                small++;
+
+        int wordLength = 0;
+        for (int i = offset; i < offset + BYTES_PER_THREAD; i++) {
+            if (bytes[i] == 32) { //space character
+                if (wordLength > 0) {
+                    if (wordLength > 4) {
+                        big++;
+                    } else {
+                        small++;
+                    }
+                    wordLength = 0;
+                }
             } else {
-                big++;
+                wordLength++;
             }
         }
-       /* synchronized (this) {
-            try {
-                os.writeInt(index);
-                os.writeInt(big);
-                os.writeInt(small);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
-        try {
-            lock.lock();
-            os.writeInt(index);
-            os.writeInt(big);
-            os.writeInt(small);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
+
+//        try {
+//            lock.lock();
+//            os.writeInt(index);
+//            os.writeInt(big);
+//            os.writeInt(small);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            lock.unlock();
+//        }
     }
 
     public static void main(String[] args) throws IOException {
