a184bfb8b8e8a2d1ad8cbf99963615ca28bf6229
1nsane <eugene.kstn@gmail.com>
2016-09-28 18:55:49 +0300

    true aspectj attempt

--- task7/src/main/java/edu/epam/mentoring/task7/aspect/LoggingAspect.java
+++ task7/src/main/java/edu/epam/mentoring/task7/aspect/LoggingAspect.java
@@ -2,19 +2,28 @@ package edu.epam.mentoring.task7.aspect;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * Created by eugen on 28.09.2016.
  */
+@Aspect
 public class LoggingAspect {
     private static Logger logger = Logger.getLogger(LoggingAspect.class);
 
-    public void logAnnotatedMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Pointcut("@annotation(edu.epam.mentoring.task7.annotation.Logging)")
+    public void methodsToBeProfiled(){}
+
+    @Around("methodsToBeProfiled()")
+    public Object logAnnotatedMethod(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodArg = (String)joinPoint.getArgs()[0];
         String classNameAndMethodName = joinPoint.getTarget().getClass().getSimpleName()
                 + "." + joinPoint.getSignature().getName();
         logger.info(classNameAndMethodName + "(\"" + methodArg + "\") start;");
         Object value = joinPoint.proceed();
         logger.info(classNameAndMethodName + " finish" + (value == null ? "" : " with result = " + value) + ";");
+        return value;
     }
 }
--- task7/src/main/java/edu/epam/mentoring/task7/service/SentenceWordLengthCounter.java
+++ task7/src/main/java/edu/epam/mentoring/task7/service/SentenceWordLengthCounter.java
@@ -2,9 +2,12 @@ package edu.epam.mentoring.task7.service;
 
 import edu.epam.mentoring.task7.annotation.Logging;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -13,15 +16,16 @@ import java.util.Arrays;
 /**
  * Created by eugen on 27.09.2016.
  */
+//@Component("wordCountService")
+//@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON, proxyMode = ScopedProxyMode.INTERFACES)
 public class SentenceWordLengthCounter implements Service {
-    @Resource(name = "wordCountService")
-    @Lazy
+//    @Autowired
     private Service self;
 
     @Override
     @Logging
     public void doWork(String some) {
-        Arrays.stream(some.split("\\s+")).map(self::doInternalWork).reduce(0, (e1, e2) -> e1 + e2);
+        Arrays.stream(some.split("\\s+")).map(this::doInternalWork).reduce(0, (e1, e2) -> e1 + e2);
     }
 
     @Override
